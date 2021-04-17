6dfe5ea79e2b67140c14f111b7220663d0b3e942
IamGaelTP <IamGaelTP@users.noreply.github.com>
2021-04-17 17:59:51 -0500

    Scene Changer and Home Menu

--- Assets/Scripts/IntroVideoPlayer.cs
+++ Assets/Scripts/IntroVideoPlayer.cs
@@ -6,13 +6,30 @@ using UnityEngine.Video;
 public class IntroVideoPlayer : MonoBehaviour
 {
     VideoPlayer video;
+    SceneChanger sceneManager;
 
     void Awake()
     {
         video = GetComponent<VideoPlayer>();
+        sceneManager = FindObjectOfType<SceneChanger>();
 
 
         video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "studioIntro.mp4");
         Debug.Log(video.url);
+    }
+
+    void Start()
+    {
+        video.loopPointReached += LoadScene;
+    }
+
+    void LoadScene(VideoPlayer vp)
+    {
+        Invoke("WaitSeconds", 1f);  
+    }
+
+    void WaitSeconds()
+    {
+        sceneManager.LoadHomeScene();
     }
 }
